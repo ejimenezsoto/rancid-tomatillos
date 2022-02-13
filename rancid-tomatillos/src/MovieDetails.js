85ca5249bbf69bc9b6cc80ee12655d31fe6afa71
Brian Peterson <bpeterson1579@gmail.com>
2022-02-13 12:17:34 -0700

    Add movie details component skeleton

---
+++ rancid-tomatillos/src/MovieDetails.js
@@ -0,0 +1,18 @@
+import './MovieDetails.css';
+import React, { Component } from 'react'
+
+class MovieDetails extends Component {
+  constructor() {
+    super()
+    this.state={
+      
+    }
+  }
+
+  render() {
+    return()
+  }
+}
+
+
+export default MovieDetails;
