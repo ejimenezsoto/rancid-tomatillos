fdaa734a7971693908bb9a2b838f811de4d2c32b
Enzo Jimenez-Soto <enzojimenezsoto@gmail.com>
2022-02-14 10:47:20 -0800

    ovie details styling

--- rancid-tomatillos/src/MovieDetails.js
+++ rancid-tomatillos/src/MovieDetails.js
@@ -1,5 +1,6 @@
 import './MovieDetails.css';
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
 
 class MovieDetails extends Component {
   constructor(props) {
@@ -27,6 +28,8 @@ class MovieDetails extends Component {
 
 
 
+
+
   render() {
     const movie = this.state.movie;
 
@@ -36,12 +39,17 @@ class MovieDetails extends Component {
           <img src={movie.backdrop_path} alt={`${movie.title} backdrop`} />
         </div>
         <div className='movie-content'>
+          <Link to='/' >
+            <button className='back-button'>
+              Go Back
+            </button>
+          </Link>
           <img src={movie.poster_path} alt={`${movie.title} poster`} className='movie-poster' />
           <section className='info'>
-            <h3 className='title'>Title: {movie.title}</h3>
+            <h3 className='title'>{movie.title}</h3>
             <p className='release-date'>Release Date: {movie.release_date}</p>
             <p className='rating'>Average Rating: {movie.average_rating}</p>
-            <ul>GENRE: <div className='genres'>{this.displayGenre()}</div></ul>
+            <ul className='genres'>Genre: <div className='genre-div'>{this.displayGenre()}</div></ul>
             <p className='overview'>Summary: {movie.overview}</p>
             <p className='revenue'>Revenue: ${movie.revenue}</p>
             <p className='runtime'>Runtime: {movie.runtime} minutes</p>
